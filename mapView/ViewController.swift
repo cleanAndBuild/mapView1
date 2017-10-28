1390cff440026f24b44007f1c6370dfa7e4b666f
Kobayashi Yako <yako.koba@gmail.com>
2017-10-29 01:26:53 +0900

    Initial Commit

---
+++ mapView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapView
+//
+//  Created by Kobayashi Yako on 2017/10/29.
+//  Copyright © 2017年 Step App School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
